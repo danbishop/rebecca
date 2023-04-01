54b2ae2037b66810aa80cb174f2333617795295e
Dan Bishop <d@nbishop.uk>
2023-04-01 17:39:01 +0100

    All the things

--- go.rb
+++ go.rb
@@ -1,9 +1,20 @@
 #!/usr/bin/ruby
 # frozen_string_literal: true
 
+require 'sinatra'
+require 'vlc-client'
+
+puts "Run vlc --extraintf rc --rc-host=127.0.0.1:9999"
+
+VLC = VLC::Client.new('127.0.0.1', 9999)
+
+VLC.connect
+
 SONGS = '/home/dan/Karaoke/'
 
 songs = Dir.glob("#{SONGS}*.mp3")
+
+song_db = []
 
 songs.each do |song|
   song = File.basename song
@@ -13,6 +24,18 @@ songs.each do |song|
   title = title.gsub(' [karaoke].mp3', '')
   title = title.gsub('.mp3', '')
   p "Artist: #{artist_title[0]} Title: #{title}"
+  song_db.push({ artist: artist_title[0], title: title, path: song })
 end
 
-p "Total Songs: #{songs.count}"
+set :song_db, song_db
+
+get '/' do
+  @song_db = song_db
+  erb :index
+end
+
+get '/play/:id' do
+  id = params[:id].to_i
+  puts id
+  VLC.play("/home/dan/Karaoke/#{song_db[id][:path]}")
+end
