9e36aa8541e9e91f72e3b511683e2a329d87f673
Dan Bishop <d@nbishop.uk>
2023-04-05 21:26:52 +0100

    Improve regex and allow access from network

--- go.rb
+++ go.rb
@@ -4,6 +4,9 @@
 require 'sinatra'
 require 'vlc-client'
 
+# Make available to all ips
+set :bind, '0.0.0.0'
+
 puts "Run vlc --extraintf rc --rc-host=127.0.0.1:9999"
 
 VLC = VLC::Client.new('127.0.0.1', 9999)
@@ -20,8 +23,9 @@ songs.each do |song|
   song = File.basename song
   artist_title = song.split(' - ', 2)
   title = artist_title[1]
-  title = title.gsub(' [Karaoke].mp3', '')
-  title = title.gsub(' [karaoke].mp3', '')
+  title = title.gsub(/ \[.*?\].mp3/, '')
+  # title = title.gsub(' [Karaoke].mp3', '')
+  # title = title.gsub(' [karaoke].mp3', '')
   title = title.gsub('.mp3', '')
   p "Artist: #{artist_title[0]} Title: #{title}"
   song_db.push({ artist: artist_title[0], title: title, path: song })
